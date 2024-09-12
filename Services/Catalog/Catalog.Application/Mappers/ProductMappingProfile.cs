10b7318e1deba7b29ce0e4f6ef53301e6f654268
Farhankaioum <keiuom.swe@gmail.com>
2024-09-12 11:47:14 +1000

    Mappers added in Catalog.Application

---
+++ Services/Catalog/Catalog.Application/Mappers/ProductMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Catalog.Application.Commands;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Specs;
+
+namespace Catalog.Application.Mappers
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+            CreateMap<Product, ProductResponse>().ReverseMap();
+            CreateMap<ProductType, TypesResponse>().ReverseMap();
+            CreateMap<Product, CreateProductCommand>().ReverseMap();
+            CreateMap<Pagination<Product>, Pagination<ProductResponse>>().ReverseMap();
+        }
+    }
+}
+    
