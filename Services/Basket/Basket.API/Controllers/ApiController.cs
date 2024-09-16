f09934007061536b2c079ca756ffeda2daa6b8ff
Farhankaioum <keiuom.swe@gmail.com>
2024-09-16 13:18:47 +1000

    ApiController and BasketController added in Basket.API

---
+++ Services/Basket/Basket.API/Controllers/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+    }
+}
---
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,81 @@
+﻿using Asp.Versioning;
+using Basket.Application.Commands;
+using Basket.Application.GrpcService;
+using Basket.Application.Mappers;
+using Basket.Application.Queries;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
+using EventBus.Messages.Common;
+using MassTransit;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [ApiVersion("1")]
+    public class BasketController : ApiController
+    {
+        public readonly IMediator _mediator;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(IMediator mediator, IPublishEndpoint publishEndpoint, ILogger<BasketController> logger)
+        {
+            _mediator = mediator;
+            _publishEndpoint = publishEndpoint;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]/{userName}", Name = "GetBasketByUserName")]
+        [ProducesResponseType(typeof(ShoppingCartResponse), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartResponse>> GetBasket(string userName) 
+        {
+            var query = new GetBasketByUserNameQuery(userName);
+            var basket = await _mediator.Send(query);
+            return Ok(basket);
+        }
+
+        [HttpPost("CreateBasket")]
+        [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartResponse>> UpdateBasket([FromBody] CreateShoppingCartCommand createShoppingCartCommand)
+        {
+            var basket = await _mediator.Send(createShoppingCartCommand);
+            return Ok(basket);
+        }
+
+        [HttpDelete]
+        [Route("[action]/{userName}", Name = "DeleteBasketByUserName")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> DeleteBasket(string userName) 
+        {
+            var cmd = new DeleteBasketByUserNameCommand(userName);
+            return Ok(await _mediator.Send(cmd));
+        }
+        
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            //Get the existing basket with username
+            var query = new GetBasketByUserNameQuery(basketCheckout.UserName);
+            var basket = await _mediator.Send(query);
+            if(basket == null)
+            {
+                return BadRequest();
+            }
+
+            var eventMsg = BasketMapper.Mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMsg.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMsg);
+            _logger.LogInformation($"Basket Published for {basket.UserName}");
+            //remove the basket
+            var deleteCmd = new DeleteBasketByUserNameCommand(basketCheckout.UserName);
+            await _mediator.Send(deleteCmd);
+            return Accepted();
+        }
+    }
+}
