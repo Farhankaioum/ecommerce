d93b59d33487e2a6da9b4579af000eeb1c72e8fd
Farhankaioum <keiuom.swe@gmail.com>
2024-09-16 13:02:16 +1000

    Mappers added in Basket.Application

---
+++ Services/Basket/Basket.Application/Mappers/BasketMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
+using EventBus.Messages.Common;
+using EventBus.Messages.Events;
+
+namespace Basket.Application.Mappers
+{
+    public class BasketMappingProfile : Profile
+    {
+        public BasketMappingProfile()
+        {
+            CreateMap<ShoppingCart, ShoppingCartResponse>().ReverseMap();
+            CreateMap<ShoppingCartItem, ShoppingCartItemResponse>().ReverseMap();
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+            CreateMap<BasketCheckoutV2, BasketCheckoutEventV2>().ReverseMap();
+        }
+    }
+}
