b5d82db6dc94ea67da49781043bb3a4a86b4ac9f
Farhankaioum <keiuom.swe@gmail.com>
2024-09-18 23:45:38 +1000

    Handler added in Discount.Application library project

---
+++ Services/Discount/Discount.Application/Handlers/GetDiscountQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Discount.Application.Queries;
+using Discount.Core.Repositories;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Discount.Application.Handlers
+{
+    public class GetDiscountQueryHandler : IRequestHandler<GetDiscountQuery, CouponModel>
+    {
+        private readonly IDiscountRepository _discountRepository;
+        private readonly ILogger<GetDiscountQueryHandler> _logger;
+
+        public GetDiscountQueryHandler(IDiscountRepository discountRepository, ILogger<GetDiscountQueryHandler> logger)
+        {
+            _discountRepository = discountRepository;
+            _logger = logger;
+        }
+        public async Task<CouponModel> Handle(GetDiscountQuery request, CancellationToken cancellationToken)
+        {
+            var coupon = await _discountRepository.GetDiscount(request.ProductName);
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount for the Product Name = {request.ProductName} not found")); 
+            }
+            var couponModel = new CouponModel
+            {
+                Id = coupon.Id,
+                Amount = coupon.Amount,
+                Description = coupon.Description,
+                ProductName = coupon.ProductName
+            };
+            _logger.LogInformation($"Copon for the {request.ProductName} is fetched");
+            return couponModel;
+        }
+    }
+}
