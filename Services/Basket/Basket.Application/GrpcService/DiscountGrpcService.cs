38d7f38992bc0fbb5e1257fa1af1e42791531d02
Farhankaioum <keiuom.swe@gmail.com>
2024-09-20 23:30:05 +1000

    DiscountGrpcService updated

--- Services/Basket/Basket.Application/GrpcService/DiscountGrpcService.cs
+++ Services/Basket/Basket.Application/GrpcService/DiscountGrpcService.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discount.Grpc.Protos;
 
 namespace Basket.Application.GrpcService
 {
-    internal class DiscountGrpcService
+    public class DiscountGrpcService
     {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoServiceClient;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
+        {
+            _discountProtoServiceClient = discountProtoServiceClient;
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            return await _discountProtoServiceClient.GetDiscountAsync(discountRequest);
+        }
     }
 }
