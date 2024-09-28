9f782921c464183f3da2452e0ccc0974753535bc
Farhankaioum <keiuom.swe@gmail.com>
2024-09-28 23:29:36 +1000

    Order mappping profile added in Ordering.Application library

---
+++ Services/Ordering/Ordering.Application/Mappers/OrderMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EventBus.Messages.Common;
+using EventBus.Messages.Events;
+using Ordering.Application.Commands;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+
+namespace Ordering.Application.Mappers
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            CreateMap<Order, OrderResponse>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommandV2>().ReverseMap();
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+            CreateMap<CheckoutOrderCommandV2, BasketCheckoutEventV2>().ReverseMap();
+        }
+    }
+}
