21d7894fce01824aa1b5184e99c9ee11bb8925c9
Farhankaioum <keiuom.swe@gmail.com>
2024-09-30 23:02:12 +1000

    BasketCheckout added in mapping profile in Basket.Application

--- Services/Basket/Basket.Application/Mappers/BasketMappingProfile.cs
+++ Services/Basket/Basket.Application/Mappers/BasketMappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Basket.Application.Responses;
 using Basket.Core.Entities;
+using EventBus.Message.Events;
 
 namespace Basket.Application.Mappers
 {
@@ -10,7 +11,7 @@ namespace Basket.Application.Mappers
         {
             CreateMap<ShoppingCart, ShoppingCartResponse>().ReverseMap();
             CreateMap<ShoppingCartItem, ShoppingCartItemResponse>().ReverseMap();
-            //CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
             //CreateMap<BasketCheckoutV2, BasketCheckoutEventV2>().ReverseMap();
         }
     }
