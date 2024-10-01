0e77cba988e576531db41e940d5482e16e1738ff
Farhankaioum <keiuom.swe@gmail.com>
2024-10-01 23:44:05 +1000

    Logging functionality added in Common.Logging

---
+++ Infrastructure/Common.Logging/Logging.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+
+namespace Common.Logging
+{
+    public static class Logging
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> ConfigureLogger =>
+            (context, loggerConfiguration) =>
+            {
+                var env = context.HostingEnvironment;
+                loggerConfiguration.MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ApplicationName", env.ApplicationName)
+                .Enrich.WithProperty("EnvironmentName", env.EnvironmentName)
+                .Enrich.WithExceptionDetails()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Warning)
+                .WriteTo.Console();
+            };
+    }
+}
